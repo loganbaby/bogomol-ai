1539b3165586cef9d88bc903f54ccd28ded7867c
loganbaby <shifo3456@gmail.com>
2023-05-11 23:05:19 +0300

    bot added

--- handlers/gptai.py
+++ handlers/gptai.py
@@ -1,16 +1,26 @@
 from config.config import app, chatbot
-from revChatGPT.typings import APIConnectionError
 from pyrogram import filters
+from revChatGPT.typings import Error as ChatGPTError
+from pyrogram.errors.exceptions import MessageIdInvalid
+from contextlib import suppress
+from pyrogram import types
 import pyrogram.enums.parse_mode as parse_mode
+import asyncio
 
 
-@app.on_message(filters.me)
+async def delete_message(message: types.Message, sleep_time: int = 0):
+    await asyncio.sleep(sleep_time)
+    with suppress(MessageIdInvalid):
+        await message.delete()
+
+
+@app.on_message(filters.text & filters.outgoing)
 async def bogomol_reply_private(_, message):
-    answer = ''
-
     try:
-        for data in chatbot.ask_stream(message.text):
-            answer += data
-        await app.send_message(chat_id=message.chat.id, text=answer)
-    except APIConnectionError:
-        await app.send_message(chat_id=message.chat.id, text='❎ <b><i>Запрос не выполнен<i><b>\n\nПока что мой <u>светлый</u> разум рассчитан на поток пользователей, поэтому из-за </i><b>неправильной организации</b></i> очереди запросов - 3 запроса доступны за 1 минуту пользования ботом.\n\nПросто <u>подождите немного</u> и <b>попробуйте заново</b> ✅', parse_mode=parse_mode.ParseMode.HTML)
+        response = ''
+        for data in chatbot.ask(message.text):
+            response = data['message']
+        await app.send_message(chat_id=message.chat.id, text=response)
+    except ChatGPTError:
+        msg = await app.send_message(chat_id=message.chat.id, text='❎ <b><i>Запрос не выполнен</i></b>\n\nПока что мой <u>светлый</u> разум рассчитан на поток пользователей, поэтому из-за <i><b>неправильной организации</b></i> очереди запросов - 5 запросов доступны за 10 секунд пользования ботом.\n\nПросто <u>подождите немного</u> и <b>попробуйте заново</b> ✅', parse_mode=parse_mode.ParseMode.HTML)
+        asyncio.create_task(delete_message(msg, 10))
